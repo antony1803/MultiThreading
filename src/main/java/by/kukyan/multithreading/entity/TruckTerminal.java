c768172e53f157992671ffe85dc849728d0f0f1f
antony1803 <anton.kukyan@gmail.com>
2022-03-24 09:09:03 +0300

    refactor

--- src/main/java/by/kukyan/multithreading/entity/TruckTerminal.java
+++ src/main/java/by/kukyan/multithreading/entity/TruckTerminal.java
@@ -18,7 +18,7 @@ public class TruckTerminal {
     public void uploadUnload(Truck truck) throws CustomException {
         truck.setState(TruckState.LOADING);
         try {
-            TimeUnit.MILLISECONDS.sleep(truck.isForUploading() ? 12 * (truck.getMaxCapacity() - truck.getCargoSize()) : 12 * truck.getCargoSize());
+            TimeUnit.MILLISECONDS.sleep(truck.isForUploading() ? 12 * (truck.getMaxCapacity() - truck.getCargoSize() + 10) : 12 * truck.getCargoSize() + 10);
         } catch (InterruptedException e) {
             logger.error("error while waiting for full load", e);
             Thread.currentThread().interrupt();
